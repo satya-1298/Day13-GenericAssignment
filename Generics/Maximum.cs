31cb2a54ca012734c5cad7d7a55105c2d26d756b
unknown <satya-1298>
2023-06-30 16:57:30 +0530

    [Satya]Add-Maximum of three Strings

---
+++ Generics/Maximum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Maximum
+    {
+        public string Number(string firstvalue, string   secondvalue, string thirdvalue)
+        {
+            //TestCase-1
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+            firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
+            {
+                return firstvalue;
+            }
+            //TestCase-2
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+            {
+                return secondvalue;
+            }
+            //TestCase-3
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
+            {
+                return thirdvalue;
+            }
+            return firstvalue;
+        }
+
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Maximum maximum = new Maximum();
+            Console.WriteLine("Enter the FirstNumber");
+            string firstvalue = Console.ReadLine();
+            Console.WriteLine("Enter the SecondNumber");
+            string secondvalue = Console.ReadLine();
+            Console.WriteLine("Enter the ThirdNumber");
+            string thirdvalue = Console.ReadLine();
+            string val = maximum.Number(firstvalue, secondvalue, thirdvalue);
+            Console.WriteLine("{0} is largest number ", val);
+            Console.ReadLine();
+        }
+    }
+}
