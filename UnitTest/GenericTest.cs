4897e1b4c3b76c01a1b413e873480068d0d26078
unknown <satya-1298>
2023-06-30 16:58:27 +0530

    [Satya]Add-Test case of Strings Max

---
+++ UnitTest/GenericTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Generics;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class GenericTest
+    {
+        [TestMethod]
+        public void TestMethod_One()
+        {
+            //Arrange
+           string firstvalue = "Satya";
+           string secondvalue = "Sai";
+           string thirdvalue = "Ram";
+            //Act
+            Maximum maximum = new Maximum();
+            string val = maximum.Number(firstvalue, secondvalue, thirdvalue);
+            //Assert
+            Assert.AreEqual(firstvalue, val);
+        }
+        [TestMethod]
+        public void TestMethod_Two()
+        {
+            //Arrange
+            string firstvalue = "Arya";
+            string secondvalue = "sneha";
+            string thirdvalue = "Neha";
+            //Act
+            Maximum maximum = new Maximum();
+             string val = maximum.Number(firstvalue, secondvalue, thirdvalue);
+            //Assert
+            Assert.AreEqual(secondvalue, val);
+        }
+        [TestMethod]
+        public void TestMethod_Three()
+        {
+            //Arrange
+            string firstvalue = "Charan";
+            string secondvalue = "Ram";
+            string thirdvalue = "Teja";
+            //Act
+            Maximum maximum = new Maximum();
+            string val = maximum.Number(firstvalue, secondvalue, thirdvalue);
+            //Assert
+            Assert.AreEqual(thirdvalue, val);
+        }
+
+    }
+}
+
